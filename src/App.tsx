e44590ee3cbeaefb62c6d892a99ec0a03f618c3c
tien998 <tienvn998@gmai.com>
2025-05-27 12:03:26 +0700

    init PostPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Root from './pages/Root'
 import ErrorPage from './pages/ErrorPage'
 import { lazy, Suspense } from 'react'
 
-
+const Post = lazy(() => import('./pages/Post'))
 const Login = lazy(() => import('./pages/authen/Login'))
 const Signup = lazy(() => import('./pages/authen/Signup'))
 
@@ -14,13 +14,18 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
+        index: true,
+        element: <Suspense fallback={<div>Loading...</div>}><Post /></Suspense>
+      },
+      {
         path: '/login',
         element: <Suspense fallback={<div>Loading...</div>}><Login /></Suspense>
       },
-        {
-          path: '/signup',
-          element: <Suspense fallback={<div>Loading...</div>}><Signup /></Suspense>
-        }
+      {
+        path: '/signup',
+        element: <Suspense fallback={<div>Loading...</div>}><Signup /></Suspense>
+      },
+
     ]
   }
 ])
---
+++ src/pages/Post/index.tsx
@@ -0,0 +1,14 @@
+import Button from "../../components/Button"
+import PostForm from "./comps/postForm"
+
+export default function PostPage() {
+    return (
+        <div className="flex flex-col items-center justify-center my-2">
+
+            <PostForm />
+            <Button>NEW POST</Button>
+
+        </div>
+    )
+
+}
